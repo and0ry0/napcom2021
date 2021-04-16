372d6bf3a82bf00e7bef46c6558c8a1775168925
Ryo Ando <sasigume@gmail.com>
2021-04-16 22:57:52 +0900

    prevent style injection

--- part/body/sidebar.php
+++ part/body/sidebar.php
@@ -1,3 +1,3 @@
-<div class="1000:sticky 1000:top-0 1000:h-1 mx-auto w-auto 1000:ml-auto 1000:mr-0 650:w-650 1000:w-250 1000:pl-6">
-<?php dynamic_sidebar('sidebar'); ?>
+<div style="height:max-content!important;" class="1000:sticky 1000:top-0 1000:h-1 mx-auto w-auto 1000:ml-auto 1000:mr-0 650:w-650 1000:w-250 1000:pl-6">
+  <?php dynamic_sidebar('sidebar'); ?>
 </div>
